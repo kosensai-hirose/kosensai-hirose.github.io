2fd6e9c536692e3d7bc1e7e265da213c1ae15329
kosensai-hirose <kosensaizitsui@gmail.com>
2022-10-04 01:04:32 +0900

    古いホームページのファイルの削除と近日公開ページの追加

--- docs/js/tabs.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	const params = location.search.slice(1).split("&").map(set => set.split("="));
-	
-	for(const param of params){
-		if(param[0] == "tab"){
-			$("#" + param[1]).prop("checked", true);
-		}
-	}
-});
